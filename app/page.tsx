08fa2ac17b41feafe74043c12c46343f09a425a8
Anuj <invincibleanuj1718@gmail.com>
2026-07-29 22:42:11 +0530

    Modified

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import CaseStudySection from "@/components/caseStudySection";
 import FeatureSection from "@/components/featureSection";
 import FloatingCard from "@/components/floatingCard";
+import Footer from "@/components/footerSection";
 import Hero from "@/components/hero";
 import JournalSection from "@/components/journalSection";
+import LowerSection from "@/components/lowerSection";
 import Navbar from "@/components/navbar";
+import TestimonialSection from "@/components/testimonialSection";
 import ValueSection from "@/components/valueSection";
 
 export default function Home() {
@@ -16,6 +19,9 @@ export default function Home() {
       <ValueSection />
       <CaseStudySection />
       <JournalSection />
+      <TestimonialSection />
+      <LowerSection />
+      <Footer />
     </div>
   );
 }
